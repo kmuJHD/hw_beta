1db9ea51c29c94b1abcef561663a9e5450aa5390
kmuJHD <temple@kmu.ac.kr>
2016-05-24 05:10:33 +0900

    타입 문자형으로 변경

--- types.h
+++ types.h
@@ -7,29 +7,29 @@
 
 
 //Client 에서 보내는 패킷 type 
-#define CP_QUESTION 0
-#define CP_RENEW 1
+#define CP_QUESTION '0'
+#define CP_RENEW '1'
 
 //상세수준
-#define LOW 0
-#define MID 1
-#define HIG 2
+#define LOW '0'
+#define MID '1'
+#define HIG '2'
 
 //등급
-#define FIR 0
-#define SEC 1
-#define THI 2
+#define FIR '0'
+#define SEC '1'
+#define THI '2'
 
-#define SP_ANSWER 0
-//#define SP_수정된 질문 1
-#define SP_UNI 2
+#define SP_ANSWER '0'
+//#define SP_수정된 질문 '1'
+#define SP_UNI '2'
 
-#define SP_UNI_KEYWORD 0
-#define SP_UNI_RANK 1 
+#define SP_UNI_KEYWORD '0'
+#define SP_UNI_RANK '1' 
 
-#define ANSWER_SUCCESS 0
-#define ANSWER_NOTFOUND 1
-#define ANSWER_GRADELOW 2 
+#define ANSWER_SUCCESS '0'
+#define ANSWER_NOTFOUND '1'
+#define ANSWER_GRADELOW '2' 
 
 //버프 크기
 #define BUFSIZE 1024
@@ -57,11 +57,20 @@ typedef struct SP_Answer{
 	byte type;
 	byte result;
 	byte detail;
-	byte *data;
+    byte data[BUFSIZE];
 }SP_Answer;
 
 typedef struct SP_Alternative{
     byte type;
     byte accuracy;
-    byte *data;
+    byte data[BUFSIZE];
 }SP_Alternative;
+
+typedef struct SP_RANK{
+	byte type;
+    byte renew_time[BUFSIZE];
+    byte data[BUFSIZE];
+}SP_RANK;
+
+// 연속되는 문자열 구분자
+byte TOKEN = 0x03;
